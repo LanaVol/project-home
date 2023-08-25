e2b6b9d0c2985654dd75f4a355dbaf271d4fce5a
LanaVol <svolosh4yk@gmail.com>
2023-08-25 18:31:37 +0300

    add burgerMenu & update styles with mediaScreen

--- script/scrolling.js
+++ script/scrolling.js
@@ -7,3 +7,24 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+//burger menu
+const navMenu = document.querySelector(".header__list");
+const navBurger = document.querySelector(".nav__burger");
+navBurger.addEventListener("click", showMenuBurgerOnClick);
+
+function showMenuBurgerOnClick() {
+  navMenu.classList.toggle("showNav");
+  changeCloseBurger();
+}
+
+function changeCloseBurger() {
+  if (navMenu.classList.contains("showNav")) {
+    navBurger.firstElementChild.setAttribute(
+      "src",
+      "./image/svg/closeBurger.svg"
+    );
+  } else {
+    navBurger.firstElementChild.setAttribute("src", "./image/svg/burger.svg");
+  }
+}
