211bc70089a32f1914dba0dccf62ba1af69d904d
LanaVol <svolosh4yk@gmail.com>
2023-08-28 20:35:20 +0300

    update with style chenges & modal windows

--- script/validationForm.js
+++ script/validationForm.js
@@ -1,4 +1,4 @@
-const dataFromClient = {
+let dataFromClient = {
   name: "",
   email: "",
   phone: "",
@@ -10,37 +10,28 @@ const dataFromClient = {
 const form = document.querySelector(".form");
 const requiredEls = Array.from(document.querySelectorAll(".reqEl"));
 const checkbox = document.querySelector(".checkbox");
-
-// const formCleanBtn = document.querySelector(".reservClean");
+const message = document.getElementById("message");
+const hints = Array.from(document.querySelectorAll(".hint"));
 
 checkbox.addEventListener("change", checkboxAgree);
 form.addEventListener("submit", sendFormToMail);
 form.addEventListener("click", validateForm);
-form.addEventListener("change", addData);
-
-const hints = Array.from(document.querySelectorAll(".hint"));
+message.addEventListener("change", addMessage);
 
 function checkboxAgree(e) {
   const checkflag = checkbox.nextElementSibling.firstElementChild;
   if (!e.currentTarget.checked) {
-    console.log("not checked");
     checkflag.classList.add("error");
 
     dataFromClient.agreement = false;
   } else if (e.currentTarget.checked) {
-    console.log("checked");
     checkflag.classList.remove("error");
     dataFromClient.agreement = true;
   }
 }
 
-function addData(e) {
-  if (e.target.id === "message") {
-    dataFromClient.message = e.target.value;
-  }
-  if (e.target.id === "service") {
-    dataFromClient.service = e.target.value;
-  }
+function addMessage(e) {
+  dataFromClient.message = e.target.value;
 }
 
 // functions for validate form-input on blur
@@ -62,7 +53,7 @@ function validateForm(e) {
           hints[index].style.display = "block";
         }
       } else if (input.id === "email") {
-        if (checkemail(input)) {
+        if (checkEmail(input)) {
           dataFromClient.email = input.value;
         } else {
           input.classList.add("error");
@@ -71,6 +62,13 @@ function validateForm(e) {
       } else if (input.id === "number") {
         if (checkNumber(input)) {
           dataFromClient.phone = input.value;
+        } else {
+          input.classList.add("error");
+          hints[index].style.display = "block";
+        }
+      } else if (input.id === "service") {
+        if (checkName(input)) {
+          dataFromClient.service = input.value;
         } else {
           input.classList.add("error");
           hints[index].style.display = "block";
@@ -86,19 +84,42 @@ function sendFormToMail(e) {
     dataFromClient.name !== "" &&
     dataFromClient.email !== "" &&
     dataFromClient.phone !== "" &&
+    dataFromClient.service !== "" &&
     dataFromClient.agreement
   ) {
+    showModalWindowSuccess("Success");
     console.log(dataFromClient);
+    clearForm();
+    dataFromClient = {
+      ...dataFromClient,
+      name: "",
+      email: "",
+      phone: "",
+      service: "",
+      message: "",
+      agreement: false,
+    };
   } else {
-    console.log("EEEEEE");
+    showModalWindowError(
+      "Fill in the input fields correctly and add agree check"
+    );
+    clearForm();
   }
 }
 
+// clear form fields
+function clearForm() {
+  requiredEls.forEach((el) => (el.value = ""));
+  message.value = "";
+  checkbox.checked = false;
+}
+
+// validation check
 function checkName(input) {
   return /^[a-zA-Zа-яА-ЯіІїЇґҐєЄ\s]*$/.test(input.value);
 }
 
-function checkemail(input) {
+function checkEmail(input) {
   return /^[\w\-\.]+@([\w\-]+\.)+[\w]{2,4}$/.test(input.value);
 }
 
